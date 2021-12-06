5dc26dc2f53f2e10f59677b3d436c82bdf59010f
Connor <61102529+Metater@users.noreply.github.com>
2021-12-05 19:50:00 -0500

    UNFRICK SSL, its pretty cool

--- Cat5DB/Program.cs
+++ Cat5DB/Program.cs
@@ -1,12 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// UNFRICK YOU SSL <3
+
+// https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowSpecificOrigins", builder =>
+    {
+        builder.WithOrigins("https://db.team3489.tk", "https://localhost").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.Urls.Add("http://*:80");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins");
+
+//app.Urls.Add("https://*:443");
 
 Dictionary<string, string> paths = new();
 paths.Add("root", "/");
 
+/*
 app.Use((ctx, next) =>
 {
     string url = ctx.Request.Host + ctx.Request.Path;
@@ -15,6 +36,7 @@ app.Use((ctx, next) =>
     if (!validHost || !paths.ContainsValue(ctx.Request.Path) || ctx.Request.Method != "GET") ctx.Abort();
     return next(ctx);
 });
+*/
 
 MetaDatabase db = new($"{Directory.GetCurrentDirectory()}/Cat5.db");
 Task dbTask = Task.Run(() => db.Start(1, 10000));
@@ -47,8 +69,6 @@ Task dbTask = Task.Run(() => db.Start(1, 10000));
         }
     }
 }
-
-//app.UseCertificateForwarding();
 
 // lock in really old attendance
 // fss
