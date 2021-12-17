7a0a5cb6a6dee9f58d81b48b10a2d48ea139fb63
Connor <61102529+Metater@users.noreply.github.com>
2021-12-16 22:44:29 -0500

    backend

--- Cat5DB.Publish/Program.cs
+++ Cat5DB.Publish/Program.cs
@@ -1,13 +1,17 @@
-﻿
-//var content = 
-string publishPath = @"E:\Projects\Visual Studio\Cat5Backend\Cat5DB\bin\Release\net6.0\publish";
-var appsettingsJson = new ByteArrayContent(File.ReadAllBytes($@"{publishPath}\appsettings.json"));
-var cat5DB = new ByteArrayContent(File.ReadAllBytes($@"{publishPath}\Cat5DB"));
+﻿string trusoKey = await File.ReadAllTextAsync(@"E:\Projects\Visual Studio\Cat5Backend\Cat5DB.Publish\truso.secret");
 
-string[] truso = File.ReadAllLines(@"E:\Projects\Visual Studio\Cat5Backend\Cat5DB.Publish\truso.secret");
-string trusoKey = truso[0];
+string publishPath = @"E:\Projects\Visual Studio\Cat5Backend\Cat5DB\bin\Release\net6.0\publish";
+
 HttpClient client = new();
-//string deleteRes = await client.GetStringAsync($"https://db.team3489.tk:8443/delete?key={trusoKey}");
-//Console.WriteLine($"Delete Request: {deleteRes}");
-var res = await client.PostAsync($"https://db.team3489.tk:8443/upload?key={trusoKey}&file=test.json", appsettingsJson);
-Console.WriteLine("Done");
+_ = await client.GetAsync(GetUri("delete", "file=Cat5DB/appsettings.json,Cat5DB/Cat5DB"));
+using FileStream f1 = File.Open($@"{publishPath}\appsettings.json", FileMode.Open);
+using GZipStream f1c = new(f1, CompressionMode.Compress);
+_ = await client.PostAsync(GetUri("upload", "file=Cat5DB/appsettings.json"), new StreamContent(f1));
+using FileStream f2 = File.Open($@"{publishPath}\Cat5DB", FileMode.Open);
+using GZipStream f2c = new(f1, CompressionMode.Compress);
+_ = await client.PostAsync(GetUri("upload", "file=Cat5DB/appsettings.json"), new StreamContent(f2));
+
+string GetUri(string ep, string query)
+{
+    return $"https://db.team3489.tk:8443/{ep}?key={trusoKey}&{query}";
+}
